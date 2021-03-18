4df2fe437d6a49a0b3feaf2c5f61b8a61423c0a3
korotkoves <korotkov.es@mipt.ru>
2021-03-18 22:32:07 +0300

    1.2.2

--- formation.sh
+++ formation.sh
@@ -3,5 +3,17 @@
 num=6
 model="iris"
 
-./start.sh $model $num gazebo/worlds/formation.world --ref_point 0,-72,0.1 --gazebo_ros $@
-./bin/formations_gen.py $model $num formation/borders.txt formation/test_fs/ --names T E C T &
+if [ "$1" == "prof" ]; then
+  arg="--gazebo_ros"
+elif [ "$1" == "nonprof" ]; then
+  arg=""
+else
+  echo "$0 [prof | nonprof]"
+  exit
+fi
+
+./start.sh $model $num gazebo/worlds/formation.world --ref_point 0,-72,0.1 $arg $@
+
+if [ "$1" == "prof" ]; then
+  ./bin/formations_gen.py $model $num formation/borders.txt formation/test_fs/ --names T E C T &
+fi
