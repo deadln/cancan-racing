a0bc218375f6041eed2819d3dcb2caa1ffefdc1e
korotkoves <korotkov.es@mipt.ru>
2021-03-22 22:27:56 +0300

    1.2.6

--- install.sh
+++ install.sh
@@ -16,3 +16,19 @@ fi
 if [ "$1" != "build" ]; then
  sudo apt install -y ros-noetic-gazebo-ros-control
 fi
+
+
+cws="$HOME/catkin_ws"
+rosinstall_file=""
+
+if [ ! -d $cws ];then
+  ./multiple-sitl/install/catkin_prepare.sh $cws $rosinstall_file
+  str="source $cws/devel/setup.bash"
+  rcfile=~/.bashrc
+
+  grep "$str" $rcfile >/dev/null
+  if [ $? -eq 1 ];then
+    echo $str >> $rcfile
+    source ~/.bashrc
+  fi
+fi
--- start.sh
+++ start.sh
@@ -14,12 +14,6 @@ if [ "$1" ]; then
 fi
 
 cws="$HOME/catkin_ws"
-rosinstall_file=""
-
-if [ ! -d $cws ];then
-  ./multiple-sitl/install/catkin_prepare.sh $cws $rosinstall_file
-fi
-
 
 ./stop.sh
 ./multiple-sitl/start.rb --gazebo_model $gm -n $n --firmware ./Firmware --firmware_initd ./multiple-sitl/px4 --catkin_ws $cws $@
