9e4073beafb9a0e03f78fdb9cd8a8dc16e596786
korotkoves <korotkov.es@mipt.ru>
2021-04-06 15:33:58 +0300

    fix

--- race.sh
+++ race.sh
@@ -23,5 +23,5 @@ fi
 ./start.sh $model $num gazebo/worlds/race.world $arg $@
 
 if [ "$1" == "prof" ]; then
-  ./bin/path_gen.py iris 6 race/centrals.txt race/test_ws/ 20 w1 w2 w3 w4 &
+  ./bin/path_gen.py $model $num race/centrals.txt race/test_ws/ 20 w1 w2 w3 w4 &
 fi
