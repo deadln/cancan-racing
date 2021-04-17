2b08367d4e98f325f10379616ba8512d7f1cd6b0
deadln <glebuch2000@list.ru>
2021-04-18 00:55:12 +0300

    Deleted --debug arg

--- start.sh
+++ start.sh
@@ -16,4 +16,4 @@ fi
 cws="$HOME/catkin_ws"
 
 ./stop.sh
-./multiple-sitl/start.rb --gazebo_model $gm -n $n --firmware ./Firmware --firmware_initd ./multiple-sitl/px4 --catkin_ws $cws --nolockstep --nospawn --debug $@
+./multiple-sitl/start.rb --gazebo_model $gm -n $n --firmware ./Firmware --firmware_initd ./multiple-sitl/px4 --catkin_ws $cws --nolockstep --nospawn $@
